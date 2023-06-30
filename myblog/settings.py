52338f23305a6bc3cd58bdaab369169b5ea53cbc
phanstudio <ajugapeterben@gmail.com>
2023-06-30 07:31:01 +0100

    add

--- myblog/settings.py
+++ myblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pst$jfup0pn39l9huhg@t_l7m*bi2r+bz+ue6p!a2efogpll#!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
