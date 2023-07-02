e331cf1c043e1582d8b2bd0749b3ec33606c2afe
phanstudio <ajugapeterben@gmail.com>
2023-07-02 08:39:46 +0100

    add new db

--- myblog/settings.py
+++ myblog/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'URL': os.getenv('POSTGRES_URL'),
         'NAME': os.getenv('PGNAME'),
         'USER': os.getenv('PGUSER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
         'HOST': os.getenv('PGHOST'),
         'PORT': os.getenv('PGPORT'),
     }
